50d6be4e08329c084c87654659e11f00540750b1
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-09 20:42:37 -0700

    uploading files

---
+++ js/script.js
@@ -0,0 +1,51 @@
+        
+        //Back to top button
+        const myButton = document.getElementById("button");
+        const display = document.querySelector("#button");
+
+        //Function that removes display-button class when user scrolls down and adds when scrolls up
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            myButton.classList.remove("display-button");
+            } else {
+                myButton.classList.add("display-button");
+            }
+        }
+        
+
+        //Invokes the above function
+        window.onscroll = function() {scrollFunction()};
+        
+        //Function that brings user back to top
+        function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        }
+
+        //Invokes the above function on button click
+        myButton.addEventListener("click", topFunction);
+        
+        //Mobile menu
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        hamburger.addEventListener("click", mobileMenu);
+
+        function mobileMenu() {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        }
+
+        //To close mobile menu
+        const navLink = document.querySelectorAll(".nav-link");
+
+       // navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+        for (let i = 0; i < navLink.length; i++) {
+                                                                                    navLink[i].addEventListener("click", closeMenu)
+        }
+
+        function closeMenu() {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }
